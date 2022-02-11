c0eb9a6e29764a7bb8e18b0f34b960fed72821e7
stgoneira <stgoneiran@gmail.com>
2022-02-10 21:05:00 -0300

    evento submit y click - ej. no funcional

--- js/contacto.js
+++ js/contacto.js
@@ -1,9 +1,17 @@
 function manejaFormulario(evento) {
-    evento.preventDefault();    
+    //evento.preventDefault();    
+    
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    // .... 
+    // procesarFormulario(nombre, apellido);
     console.log("onsubmit formulario contacto...");
-    return false;
+    
+    //return false;
 }
 
-document.getElementById("formulario-contacto").addEventListener("submit", manejaFormulario);
+//document.getElementById("formulario-contacto").addEventListener("submit", manejaFormulario);
 
-// JAVA != Javascript 
+
+document.getElementById("boton").addEventListener("click", manejaFormulario);
+
